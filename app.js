41302a4ca9d21a403dc2ca3894ec4c3a5be374a3
irinafilipenko <73819257+irinafilipenko@users.noreply.github.com>
2021-09-25 22:53:50 +0300

    heroko

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+const DB_HOST =
+  'mongodb+srv://irina:QevMF0Z5nWD3gmvZ@cluster0.qabp5.mongodb.net/db-contacts?retryWrites=true&w=majority'
+
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('HI')
+  })
+  .catch((error) => {
+    console.log(error.message)
+  })
 
 const contactsRouter = require('./routes/api/contacts')
 
@@ -23,3 +39,7 @@ app.use((err, req, res, next) => {
 })
 
 module.exports = app
+
+//QevMF0Z5nWD3gmvZ irina
+
+//mongodb+srv://irina:QevMF0Z5nWD3gmvZ@cluster0.qabp5.mongodb.net/db-contacts?retryWrites=true&w=majority
